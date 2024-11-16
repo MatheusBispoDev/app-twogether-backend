edc3bc9a03ea5b7218b612015cfbecf59756f5d7
Matheus Bispo <matheus.bispo@totvs.com.br>
2024-11-16 16:29:10 -0300

    initial commit

---
+++ us-twogether/src/main/java/com/app/us_twogether/UsTwogetherApplication.java
@@ -0,0 +1,13 @@
+package com.app.us_twogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsTwogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsTwogetherApplication.class, args);
+	}
+
+}
