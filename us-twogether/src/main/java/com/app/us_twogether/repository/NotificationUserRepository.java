4b0208a4f75cf02fe8dd93be5ca6046d5efe1c6a
Matheus Bispo <matheus.bispo@totvs.com.br>
2024-11-16 22:57:54 -0300

    🚀 Inclusão do relacionamento com o NotificationUser

---
+++ us-twogether/src/main/java/com/app/us_twogether/repository/NotificationUserRepository.java
@@ -0,0 +1,6 @@
+package com.app.us_twogether.repository;
+
+import com.app.us_twogether.model.NotificationUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationUserRepository extends JpaRepository<NotificationUser, Integer> {}
