ef411ab8509cae5eeb37471d1b9680b168665b3b
Matheus Bispo <matheus.bispo@totvs.com.br>
2024-11-18 13:42:45 -0300

    🚀 US-34 Retirada de campo de cpf e validações relacionadas

--- us-twogether/src/main/java/com/app/us_twogether/dto/UserDTO.java
+++ us-twogether/src/main/java/com/app/us_twogether/dto/UserDTO.java
@@ -2,13 +2,10 @@ package com.app.us_twogether.dto;
 
 import com.app.us_twogether.model.NotificationUser;
 
-public class UserDTO {
-    private String username;
-    private NotificationUser notificationUser;
-    private String password;
-    private String name;
-    private String email;
-    private String phoneNumber;
-    private String cpf;
-    private String type;
-}
+public record UserDTO(String username,
+         NotificationUser notificationUser,
+         String password,
+         String name,
+         String email,
+         String phoneNumber,
+         String type) {}
--- us-twogether/src/main/java/com/app/us_twogether/repository/UserRepository.java
+++ us-twogether/src/main/java/com/app/us_twogether/repository/UserRepository.java
@@ -2,9 +2,8 @@ package com.app.us_twogether.repository;
 
 import com.app.us_twogether.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> findBycpf(String cpf);
+    UserDetails findByUsername(String username);
 }
