8c3b7f843020b37ead1f48253a8a6dfdfec3c4ea
Matheus Bispo <matheus.bispo@totvs.com.br>
2024-11-18 14:49:39 -0300

    🚀 Retirada de campo de cpf e validações relacionadas

--- us-twogether/src/main/java/com/app/us_twogether/exception/CPF/CPFInvalidException.java
+++
@@ -1,7 +0,0 @@
-package com.app.us_twogether.exception.CPF;
-
-public class CPFInvalidException extends RuntimeException {
-    public CPFInvalidException(String message) {
-        super(message);
-    }
-}
--- us-twogether/src/main/java/com/app/us_twogether/exception/GlobalExceptionHandler.java
+++ us-twogether/src/main/java/com/app/us_twogether/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.app.us_twogether.exception;
 
-import com.app.us_twogether.exception.CPF.CPFInvalidException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,12 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
     @ExceptionHandler(DataAlreadyExistsException.class)
     public ResponseEntity<String> handleDataAlreadyExistsException(DataAlreadyExistsException ex) {
-        return ResponseEntity
-                .status(HttpStatus.CONFLICT) // 409 Conflict
-                .body(ex.getMessage());
-    }
-    @ExceptionHandler(CPFInvalidException.class)
-    public ResponseEntity<String> handleCPFInvalidException(CPFInvalidException ex) {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT) // 409 Conflict
                 .body(ex.getMessage());
