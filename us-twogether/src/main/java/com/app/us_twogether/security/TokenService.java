ae2ff5489d5bcea5835b46f6e66ae5a5cfa91a3f
Matheus Bispo <matheus.bispo@totvs.com.br>
2024-11-21 11:37:06 -0300

    🚀 US-34 Validação para quando o token estiver inválido

--- us-twogether/src/main/java/com/app/us_twogether/security/TokenService.java
+++ us-twogether/src/main/java/com/app/us_twogether/security/TokenService.java
@@ -1,15 +1,12 @@
 package com.app.us_twogether.security;
 
-import com.app.us_twogether.model.User;
+import com.app.us_twogether.domain.user.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
@@ -33,6 +30,8 @@ public class TokenService {
             Algorithm algorithm = Algorithm.HMAC256(secret);
 
             return JWT.require(algorithm).withIssuer("ustwogether-auth-api").build().verify(token).getSubject();
+        } catch (TokenExpiredException exception) {
+            return "O token expirou. Por favor, faça login novamente. " + exception.getMessage();
         } catch (JWTCreationException exception) {
             return "";
         }
