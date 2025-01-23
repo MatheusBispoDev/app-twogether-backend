e5739423dd5d400f5f85c949123e1181ba779785
Matheus Bispo <matheus.bispo@totvs.com.br>
2025-01-23 10:09:26 -0300

    🚀 US-38 Implementação do CRUD de Sub Category

--- us-twogether/src/main/java/com/app/us_twogether/domain/category/SubCategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.app.us_twogether.domain.category;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
-}
