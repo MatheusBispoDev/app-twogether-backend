5ebac7b878b20fe374a506a4132f359dd4a51141
Matheus Bispo <matheus.bispo@totvs.com.br>
2025-01-24 20:34:09 -0300

    🚀 US-38 Implementação do TaskRequestDTO para melhor validação de envio de dados

---
+++ us-twogether/src/main/java/com/app/us_twogether/domain/task/TaskRequestDTO.java
@@ -0,0 +1,9 @@
+package com.app.us_twogether.domain.task;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record TaskRequestDTO(String userResponsible, Long categoryId, Long subCategoryId, String title,
+                             String description, String observation, LocalDate dateCompletion, LocalTime timeCompletion, LocalDate dateEnd, LocalTime timeEnd,
+                             String attachment, boolean completed) {
+}
