0d7e8034081f3cd5d0b118923e05209c76a78776
Matheus Bispo <matheus.bispo@totvs.com.br>
2025-01-25 15:59:15 -0300

    🚀 US-71 Melhorias de código

--- us-twogether/src/main/java/com/app/us_twogether/security/TokenService.java
+++ us-twogether/src/main/java/com/app/us_twogether/security/TokenService.java
@@ -18,9 +18,8 @@ public class TokenService {
     public String generateToken(User user){
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-            String token = JWT.create().withIssuer("ustwogether-auth-api").withSubject(user.getUsername()).sign(algorithm);
 
-            return token;
+            return JWT.create().withIssuer("ustwogether-auth-api").withSubject(user.getUsername()).sign(algorithm);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Error while generation token", exception);
         }
