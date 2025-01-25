c89a4bff34c8a7be3f1f55fe314bf3f01c5c0440
Matheus Bispo <matheus.bispo@totvs.com.br>
2025-01-25 16:57:09 -0300

    🚀 US-69 Mudança de nomenclatura

--- us-twogether/src/main/java/com/app/us_twogether/domain/authentication/AuthenticationDTO.java
+++
@@ -1,4 +0,0 @@
-package com.app.us_twogether.domain.authentication;
-
-public record AuthenticationDTO(String username, String password) {
-}
---
+++ us-twogether/src/main/java/com/app/us_twogether/domain/authentication/AuthenticationRequestDTO.java
@@ -0,0 +1,3 @@
+package com.app.us_twogether.domain.authentication;
+
+public record AuthenticationRequestDTO(String username, String password) { }
