95adb4faa8305418170f078f837fce84eac6eefc
Matheus Bispo <matheus.bispo@totvs.com.br>
2025-01-25 16:30:55 -0300

    🚀 US-71 Melhorias de codificação

--- us-twogether/src/main/java/com/app/us_twogether/domain/space/SpaceRepository.java
+++ us-twogether/src/main/java/com/app/us_twogether/domain/space/SpaceRepository.java
@@ -1,9 +1,6 @@
 package com.app.us_twogether.domain.space;
 
-import com.app.us_twogether.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,6 +8,4 @@ import java.util.Optional;
 @Repository
 public interface SpaceRepository extends JpaRepository<Space, Long> {
     Optional<Space> findBySharedToken(String sharedToken);
-    @Query("SELECT spc FROM Space spc JOIN UserSpaceRole usr ON spc = usr.space WHERE usr.user = :user AND usr.accessLevel = 'US'")
-    Optional<Space> findByUserOwnerSpace(@Param("user") User user);
 }
