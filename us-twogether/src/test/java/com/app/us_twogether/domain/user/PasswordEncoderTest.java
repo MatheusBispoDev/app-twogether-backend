c313a2e3e84865c859de5d7a1deef727ce2454d9
Matheus Bispo <matheus.bispo@totvs.com.br>
2025-02-01 14:09:03 -0300

    🚀 US-46 Retirada comentários

--- us-twogether/src/test/java/com/app/us_twogether/domain/user/PasswordEncoderTest.java
+++ us-twogether/src/test/java/com/app/us_twogether/domain/user/PasswordEncoderTest.java
@@ -16,13 +16,10 @@ public class PasswordEncoderTest {
         String rawPassword = "mySecretPassword";
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
-        // Verifica se o hash não é nulo
         assertNotNull(encodedPassword);
 
-        // Verifica se a senha bruta corresponde ao hash gerado
         assertTrue(passwordEncoder.matches(rawPassword, encodedPassword));
 
-        // Opcional: garante que encodes diferentes são únicos
         String anotherEncodedPassword = passwordEncoder.encode(rawPassword);
         assertNotEquals(encodedPassword, anotherEncodedPassword);
     }
@@ -34,10 +31,8 @@ public class PasswordEncoderTest {
         String rawPassword = "mySecretPassword";
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
-        // Verifica se a senha corresponde ao hash gerado
         assertTrue(passwordEncoder.matches(rawPassword, encodedPassword));
 
-        // Testa com uma senha errada
         String wrongPassword = "wrongPassword";
         assertFalse(passwordEncoder.matches(wrongPassword, encodedPassword));
     }
