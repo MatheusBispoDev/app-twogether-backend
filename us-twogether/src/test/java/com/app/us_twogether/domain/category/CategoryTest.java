8a53830d2ad78eab1f493d6997c0a07dbb084ef7
Matheus Bispo <matheus.bispo@totvs.com.br>
2025-02-01 14:08:10 -0300

    🚀 US-46 Adicionado CategoryTest

---
+++ us-twogether/src/test/java/com/app/us_twogether/domain/category/CategoryTest.java
@@ -0,0 +1,37 @@
+package com.app.us_twogether.domain.category;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class CategoryTest {
+
+    @Test
+    public void testCreateCategory(){
+
+    }
+
+    @Test
+    public void testUpdateCategory(){
+
+    }
+
+    @Test
+    public void testDeleteCategory(){
+
+    }
+
+    @Test
+    public void testGetCategory(){
+
+    }
+
+    @Test
+    public void testGetAllCategoryFromSpace(){
+
+    }
+}
