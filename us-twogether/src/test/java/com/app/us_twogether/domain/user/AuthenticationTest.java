ec9ae221ed7cbaa0149dfea00fafe8468f0a0418
Matheus Bispo <matheus.bispo@totvs.com.br>
2025-02-01 14:11:38 -0300

    🚀 US-46 Criação de test relacionado ao Register de um usuário

--- us-twogether/src/test/java/com/app/us_twogether/domain/user/AuthenticationTest.java
+++ us-twogether/src/test/java/com/app/us_twogether/domain/user/AuthenticationTest.java
@@ -1,22 +1,34 @@
 package com.app.us_twogether.domain.user;
 
 import com.app.us_twogether.domain.authentication.token.TokenService;
+import com.app.us_twogether.exception.DataAlreadyExistsException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.*;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 @Transactional
 public class AuthenticationTest {
 
@@ -29,8 +41,26 @@ public class AuthenticationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
+    @MockBean
     private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserController userController;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Value("${app.api.base-url}")
+    private String apiBaseUrl;
+
+    private final UserRequestDTO userRequestDTO = new UserRequestDTO("john_doe", "1234", "John Doe", "john@example.com", "11932178425", "US");
+
+    private final UserResponseDTO userResponseDTO = new UserResponseDTO("john_doe", "John Doe", "john@example.com", "11932178425", "US");
+
+    @MockBean
+    private BCryptPasswordEncoder passwordEncoder;
 
     @Test
     public void testLoginUser() throws Exception {
@@ -61,28 +91,37 @@ public class AuthenticationTest {
         mockMvc.perform(post("/auth/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(loginRequest))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.token").value(token));
+                .andExpect(status().isOk());
     }
 
     @Test
-    public void testRegisterUser() throws Exception {
-        // JSON de entrada
-        String registerRequest = """
-        {
-            "username": "john_doe",
-            "password": "1234",
-            "name": "John Doe",
-            "email": "john@example.com",
-            "phoneNumber": "11932178425",
-            "type": "US"
-        }
-        """;
+    public void testRegisterUserSuccessfully() throws Exception {
+        when(userService.createUser(Mockito.any(User.class))).thenReturn(userResponseDTO);
 
-        // Execução da requisição e verificação da resposta
-        mockMvc.perform(post("/auth/register")
+        mockMvc.perform(post(apiBaseUrl + "/auth/register")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(registerRequest))
-                .andExpect(status().isOk());
+                        .content(objectMapper.writeValueAsString(userRequestDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("john_doe"))
+                .andExpect(jsonPath("$.name").value("John Doe"))
+                .andExpect(jsonPath("$.email").value("john@example.com"))
+                .andExpect(jsonPath("$.phoneNumber").value("11932178425"))
+                .andExpect(jsonPath("$.type").value("US"));
+
+        Mockito.verify(userService, times(1)).createUser(Mockito.any(User.class));
+    }
+
+    @Test
+    public void testErrorWhenUserAlreadyExists() throws Exception {
+        when(userService.createUser(Mockito.any(User.class)))
+                .thenThrow(new DataAlreadyExistsException("Usuário 'john_doe' já está cadastrado."));
+
+        mockMvc.perform(post(apiBaseUrl + "/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userRequestDTO)))
+                .andExpect(status().isConflict())
+                .andExpect(content().string("Usuário 'john_doe' já está cadastrado."));
+
+        Mockito.verify(userService, times(1)).createUser(Mockito.any(User.class));
     }
 }
