a56806e715ea34f92c5e5d719973f483958e5940
Matheus Bispo <matheus.bispo@totvs.com.br>
2025-02-12 09:16:52 -0300

    🚀 US-46 Criação dos testes da Category

--- us-twogether/src/test/java/com/app/us_twogether/domain/category/CategoryTest.java
+++ us-twogether/src/test/java/com/app/us_twogether/domain/category/CategoryTest.java
@@ -1,37 +1,83 @@
 package com.app.us_twogether.domain.category;
 
+import com.app.us_twogether.domain.space.SpaceService;
 import jakarta.transaction.Transactional;
+import org.apache.kafka.common.errors.ResourceNotFoundException;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
-@AutoConfigureMockMvc
 @Transactional
 public class CategoryTest {
 
-    @Test
-    public void testCreateCategory(){
+    @Autowired
+    private CategoryService categoryService;
 
+    @Test
+    public void shouldCreateCategory_whenCredentialsAreValid() {
+        CategoryRequestDTO categoryDTO = new CategoryRequestDTO("Work", "#FF5733", CategoryType.EXPENSE);
+
+        CategoryResponseDTO response = categoryService.createCategory(1L, categoryDTO);
+
+        assertNotNull(response);
+        assertNotNull(response.categoryId());
+        assertEquals("Work", response.title());
+        assertEquals("#FF5733", response.color());
+        assertEquals(CategoryType.EXPENSE, response.categoryType());
     }
 
     @Test
-    public void testUpdateCategory(){
+    public void shouldUpdateCategory_whenCredentialsAreValid(){
+        CategoryResponseDTO response = categoryService.createCategory(1L, new CategoryRequestDTO("Work", "#FF5733", CategoryType.EXPENSE));
 
+        categoryService.updateCategory(response.categoryId(), new CategoryRequestDTO("Home", "#FF5734", CategoryType.ACTIVITY));
+
+        Category category = categoryService.getCategory(response.categoryId());
+
+        assertNotNull(category);
+        assertNotNull(category.getCategoryId());
+        assertEquals("Home", category.getTitle());
+        assertEquals("#FF5734", category.getColor());
+        assertEquals(CategoryType.ACTIVITY, category.getCategoryType());
     }
 
     @Test
-    public void testDeleteCategory(){
+    public void shouldDeleteCategory_whenCredentialsAreValid(){
+        CategoryResponseDTO response = categoryService.createCategory(1L, new CategoryRequestDTO("Work", "#FF5733", CategoryType.EXPENSE));
 
+        categoryService.deletedCategory(response.categoryId());
+
+        assertThrows(ResourceNotFoundException.class, () -> categoryService.getCategory(response.categoryId()));
     }
 
     @Test
-    public void testGetCategory(){
+    public void shouldGetAllCategoryFromSpace(){
+        List<CategoryRequestDTO> categoriesToCreate = List.of(
+                new CategoryRequestDTO("Work", "#FF5733", CategoryType.EXPENSE),
+                new CategoryRequestDTO("Home", "#FF5733", CategoryType.EXPENSE),
+                new CategoryRequestDTO("Shopping", "#FF5733", CategoryType.EXPENSE),
+                new CategoryRequestDTO("Hospital", "#FF5733", CategoryType.EXPENSE),
+                new CategoryRequestDTO("Family", "#FF5733", CategoryType.EXPENSE)
+        );
 
-    }
+        categoriesToCreate.forEach(category -> categoryService.createCategory(1L, category));
 
-    @Test
-    public void testGetAllCategoryFromSpace(){
+        List<CategoryResponseDTO> categories = categoryService.getAllCategoriesFromSpace(1L);
+
+        assertNotNull(categories, "A lista de categorias não deveria ser nula");
+        assertEquals(5, categories.size(), "O número de categorias deveria ser 5");
+        List<String> actualTitles = categories.stream()
+                .map(CategoryResponseDTO::title)
+                .toList();
+
+        List<String> expectedTitles = List.of("Work", "Home", "Shopping", "Hospital", "Family");
+
+        assertTrue(actualTitles.containsAll(expectedTitles), "A lista de categorias deve conter os títulos esperados");
 
     }
 }
